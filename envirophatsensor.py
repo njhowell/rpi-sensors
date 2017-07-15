63827788702579362ac0535c7923489b77cf66de
Nick Howell <nick@nickhowell.co.uk>
2017-07-15 12:20:36 +0100

    sometimes i wonder if i actually have a brain

--- envirophatsensor.py
+++ envirophatsensor.py
@@ -14,13 +14,13 @@ while True:
     temp = weather.temperature()-10 
     logging.info("Got temperature of %s", str(temp))
     
-    light = light.light()
-    logging.info("Got light level of %s", str(light))
+    lightlevel = light.light()
+    logging.info("Got light level of %s", str(lightlevel))
     
     LogToPRTG.LogValue("Temperature", str(temp), envirophatconfig.temp_api_key, envirophatconfig.temp_port, envirophatconfig.prtg_host, envirophatconfig.use_ssl)
     logging.info("Logged temperature with PRTG at %s using apikey %s", envirophatconfig.prtg_host, envirophatconfig.temp_api_key)
 
-    LogToPRTG.LogValue("Light Level", str(light), envirophatconfig.light_api_key, envirophatconfig.light_port, envirophatconfig.prtg_host, envirophatconfig.use_ssl)
+    LogToPRTG.LogValue("Light Level", str(lightlevel), envirophatconfig.light_api_key, envirophatconfig.light_port, envirophatconfig.prtg_host, envirophatconfig.use_ssl)
     logging.info("Logged light level with PRTG at %s using apikey %s", envirophatconfig.prtg_host, envirophatconfig.light_api_key)
 
     logging.info("Going to sleep for %s seconds", interval)
